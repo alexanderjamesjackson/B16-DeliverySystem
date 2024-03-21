ed36903ce73e917a94a91b13a0a8c1a8f114dbba
j44ck00 <alex.jackson@new.ox.ac.uk>
2024-03-21 21:48:22 +0000

    First commit

---
+++ maps.hpp
@@ -0,0 +1,21 @@
+#ifndef MAPS_HPP
+#define MAPS_HPP
+
+
+#include <vector>
+
+class Graph{
+    private:
+        std::vector < std::vector<float>> graph;
+        int n;
+
+    public:
+        Graph(int N);
+};
+
+
+
+
+
+
+#endif 
